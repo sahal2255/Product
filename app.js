58fbe178c7c123609626b28f261b883d7faa375c
sahal2255 <sahalvv9656@gmail.com>
2025-01-14 19:43:39 +0530

    vercel json file added

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const express=require('express')
 const app=express()
 
 
+app.get('/',(req,res)=>{
+    res.send('Testing the API')
+})
+
 app.listen(3000,()=>{
     console.log('server running ')
 })
