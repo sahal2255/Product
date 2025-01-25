c987d3e79f2f4b08965ed0f82bb78fdc8e91336a
sahal2255 <sahalvv9656@gmail.com>
2025-01-25 11:24:30 +0530

    dome

--- middlewares/dbConnect.js
+++ middlewares/dbConnect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const dbConnect = async () => {
   try {
-    await mongoose.connect('mongodb+srv://sahalvv9656:31wBdmjPviqgt6jZ@cluster0.on2wm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
